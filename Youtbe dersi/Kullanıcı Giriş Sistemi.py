9f905687692d185746aa16723aec2b9ab67c7201
hbyrm881 <107509024+hbyrm881@users.noreply.github.com>
2025-04-30 18:37:14 +0300

    Create Kullanıcı Giriş Sistemi.py

---
+++ Youtbe dersi/Kullanıcı Giriş Sistemi.py
@@ -0,0 +1,40 @@
+kk = ["Hasan","Yusuf"]
+kp = ["123","456"]
+print("Sisteme giriş yapmak: G kaydolmak: K  :")
+secim = input()
+
+if secim == "K":
+    ka = input("Kullanıcı adınız? :")
+    pa = input("Parolanız? :")
+    print("kaydolma başarılı şimdi giriş yapınız")
+
+    print("Giriş yapınız")
+    ka2 = input("\nKullanıcı adınız?: ")
+    pa2 = input("\nParolanız?: ")
+
+    if ka == ka2:
+        if pa == pa2:
+            print("Giriş Başarılı")
+        else:
+            print("Kullanıcı adı veya parola hatalı")
+    elif pa == pa2:
+        print("Kullanıcı adı veya parola hatalı")
+    else:
+        print("Kullanıcı adı veya parola hatalı")
+
+elif secim == "G":
+    gk = input("Kullanıcı adınız? :")
+    gp = input("Parolanız? :")
+
+    if kk == gk:
+        if kp == gp:
+            print("Giriş Başarılı")
+        else:
+            print("Kullanıcı adı veya parola hatalı")
+    elif kk != gk:
+        print("Kullanıcı adı veya parola hatalı")
+    else:
+        print("Kullanıcı adı veya parola hatalı")
+
+else:
+    print("Yanlış tuşlama yaptınız büyük küçük harfe dikkat edin")
