a62369375073192f8b829df86a7ac2db4d1c8c91
hbyrm881 <107509024+hbyrm881@users.noreply.github.com>
2025-05-02 13:10:32 +0300

    Update tkinter_calısma.py

--- kafama göre/tkinter_calısma.py
+++ kafama göre/tkinter_calısma.py
@@ -7,6 +7,9 @@ pencere.geometry("300x500")
 entry = tk.Entry(pencere)
 entry.pack(pady=20)
 
+buton_cerceve = tk.Frame(pencere)
+buton_cerceve.pack(pady=10)
+
 listbox = tk.Listbox(pencere, selectmode="multiple", width=40)
 listbox.pack(pady=5)
 
@@ -39,12 +42,12 @@ def tumu_sil ():
         print("Herşey silindi")
     else:
         print("Liste zaten boş")
-        
-buton_ekle = tk.Button(pencere, text="Ekle",width=15, height=2,command=ekle)  
-buton_ekle.pack()  
 
-buton_sil = tk.Button(pencere, text="Sil",width=15, height=2, command=sil)
-buton_sil.pack()
+buton_ekle = tk.Button(buton_cerceve, text="Ekle",width=15, height=2,command=ekle)  
+buton_ekle.pack(side="left",padx=5)  
+
+buton_sil = tk.Button(buton_cerceve, text="Sil",width=15, height=2, command=sil)
+buton_sil.pack(side="left",padx=5)
 
 buton_allsil = tk.Button(pencere, text="Hepsini sil",width=15, height=2, command=tumu_sil)
 buton_allsil.pack()
